d4c03e61c3aa5d67a5446294c5b6973efc741138
Jose Lima <jalimag0206@gmail.com>
2023-04-29 20:32:55 -0300

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataEvento = new Date("Feb 6, 2024 13:00:00");
+const timeStampDoEvento = dataEvento.getTime();
+
+const contaHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distancia = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMS = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distancia / diasEmMS );
+    const horasAteOEvento = Math.floor((distancia % diasEmMS) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distancia % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distancia % minutoEmMs) / 1000)
+
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if(distancia < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado!'
+    }
+}, 1000)
